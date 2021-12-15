f564f1b182593184dbf239c7d67899bd642c1552
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2021-12-15 18:39:16 +0200

    add fetching from jsonplaceholder

--- pages/about.js
+++ pages/about.js
@@ -6,8 +6,9 @@ import Button from '@mui/material/Button';
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
+import axios from "axios"
 
-export default function About() {
+const About = ({users, posts}) => {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
@@ -17,9 +18,38 @@ export default function About() {
         <Button variant="contained" component={Link} noLinkStyle href="/">
           Here will be site of kosht
         </Button>
+        {users.map(item => (
+          <div key={item.id} style={{ padding: 20, margin: '10px 0', border: '1px solid blue', borderRadius: 5 }}>
+            <h3>{item.name}</h3>
+            <Typography component="p">{item.email}</Typography>
+          </div>
+        ))}
+
+        {posts.map(item => (
+          <div key={item.id} style={{ padding: 20, margin: '10px 0', border: '1px solid red', borderRadius: 5 }}>
+            <h3>{item.title}</h3>
+            <Typography component="p">{item.body}</Typography>
+          </div>
+        ))}
+
         <ProTip />
         <Copyright />
       </Box>
     </Container>
   );
+}
+
+// console.log(posts)
+
+export default About;
+
+export async function getStaticProps(context) {
+  const fetchedUsers = await axios.get('https://jsonplaceholder.typicode.com/users')
+  const fetchedPosts = await axios.get('https://jsonplaceholder.typicode.com/posts')
+  const users = fetchedUsers.data
+  const posts = fetchedPosts.data
+
+  return {
+    props: {users, posts}, // will be passed to the page component as props
+  }
 }

47d9e13b5770dec3d97e846d6957bc7e89ee24c3
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2022-01-21 03:15:42 +0200

    changed LeftMenu

--- components/UI/LeftMenu.js
+++ components/UI/LeftMenu.js
@@ -12,6 +12,7 @@ export default function LeftMenu() {
   const { t } = useTranslation('common')
   const fetcher = url => axios.get(url).then(res => res.data)
   const BASE_API_PATH = "https://kosht-api.herokuapp.com/api"
+  // const BASE_API_PATH = "http://193.46.199.82:5000/api"
 
   const {data: categories, error: categoriesError} = useSWR(`${BASE_API_PATH}/categories`, fetcher)
   const {data: contacts, error: contactsError}     = useSWR(`${BASE_API_PATH}/contacts`, fetcher)
@@ -47,16 +48,16 @@ export default function LeftMenu() {
       <LeftMenuList 
         items={categories} 
         locale={locale} 
-        item="category" 
-      /> 
+        item="category"
+        />  
       <LeftMenuList 
         items={tags} 
         locale={locale} 
         item="tag" 
-      />
+        />
       <SectionTitle 
         title={t('leftMenu.followUs')} 
-      />
+        />
       <LeftMenuList 
         items={contacts.data} 
         locale={locale} 
